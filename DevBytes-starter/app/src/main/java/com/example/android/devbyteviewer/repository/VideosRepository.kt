eb840a020fc7b44a9a15522ff0d7e7305139fa04
Omas Ajir <jicintech@gmail.com>
2019-10-26 10:23:28 +0100

    implemented a repository

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -1,2 +1,33 @@
 
 package com.example.android.devbyteviewer.repository
+
+import android.view.animation.Transformation
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.DevByteVideo
+import com.example.android.devbyteviewer.network.DevByteNetwork
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+
+/**
+ * Repository for fetching devbytes videos from network and storing on desk
+ */
+
+class VideosRepository (private val database: VideosDatabase){
+    val videos : LiveData<List<DevByteVideo>> = Transformations.map(database.videoDao.getVideos()){
+        it.asDomainModel()
+    }
+
+
+    suspend fun refreshVideos(){
+        withContext(Dispatchers.IO){
+          Timber.d("refresh videos is called")
+          val playlist = DevByteNetwork.devbytes.getPlaylist().await()
+          database.videoDao.insertAll(playlist.asDatabaseModel())
+        }
+    }
+}
